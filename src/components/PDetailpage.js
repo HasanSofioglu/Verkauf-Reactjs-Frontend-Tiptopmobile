b9d5825d287a74c020016f3547c7fde664df2fda
Hasan Sofioğlu <h.sofioglu21@gmail.com>
2022-10-26 15:51:02 +0200

    test

--- src/components/PDetailpage.js
+++ src/components/PDetailpage.js
@@ -79,7 +79,7 @@ const [SelectedPhoneName,setSelectedPhoneName]=useState("")
   useEffect(()=>{
 
     async function get (){
-      await  Axios.get("http://139.59.139.115:3001/api/detail/"+phoneId).then((response)=>{
+      await  Axios.get("http://159.223.22.74:3001/api/detail/"+phoneId).then((response)=>{
   
         setPhoneBrandList(response.data);
         console.log(response.data[0]?.PhonesName)
