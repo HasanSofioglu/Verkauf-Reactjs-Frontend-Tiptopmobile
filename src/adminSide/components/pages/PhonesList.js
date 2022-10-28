295f6f6ab56df32256e39e82ac0b6326fd73a7b8
Hasan Sofioğlu <h.sofioglu21@gmail.com>
2022-10-28 16:05:25 +0200

    test

--- src/adminSide/components/pages/PhonesList.js
+++ src/adminSide/components/pages/PhonesList.js
@@ -11,11 +11,11 @@ const[phoneModelList, setPhoneBrandList] = useState([])
 
 
 
-
-const handleClick = async (id) => {
+async function deletePhone(id) {
  
   await Axios.delete('http://161.35.64.20:3001/api/delete/'+id)
 
+  return console.log(id)
 }
 
  useEffect(()=>{
@@ -78,7 +78,7 @@ fetchMyAPI()
       </div>
       
       <div>
-        <a href="/PhonesList" onClick={handleClick(val.id)}className="button icon-button"  aria-label="Icon-only Button" viewBox="0 0 32 32" style={{backgroundColor : 'red'}}>
+        <a href="/PhonesList" onClick={deletePhone(val.id)}className="button icon-button"  aria-label="Icon-only Button" viewBox="0 0 32 32" style={{backgroundColor : 'red'}}>
           <svg xmlns="http://www.w3.org/2000/svg"  aria-hidden="true" className="icon-button__icon"  focusable="false">
             <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z" />
           </svg>
