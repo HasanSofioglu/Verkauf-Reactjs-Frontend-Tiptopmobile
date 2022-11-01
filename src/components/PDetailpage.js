ed31ee2ef189429c031d07a22ecafedb6f584e80
Hasan Sofioğlu <h.sofioglu21@gmail.com>
2022-11-01 16:12:11 +0100

    tests

--- src/components/PDetailpage.js
+++ src/components/PDetailpage.js
@@ -1,299 +1,261 @@
-const bodyParser = require('body-parser');
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require("cookie-parser");
-const app = express()
+import React,{ useState, useEffect } from 'react'
+import './pdetail.css';
+import dhllogo from "../assets/dhl_Logo.png";
+import paypal from "../assets/PayPal.png";
+import banktrans from "../assets/banktrans.png";
+import { useParams } from "react-router-dom";
+import Axios from "axios";
 
-const mysql = require('mysql');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
+import ReCAPTCHA from "react-google-recaptcha";
+import {  useNavigate} from "react-router-dom";
+
+const PDetailpage = () => {
+
+  const navigate = useNavigate();
+  const [show,setShow]=useState(true)
+
+
+ const  [imgshow,setimgShow]=useState(paypal)    
+ const { phoneId } = useParams();
+ const [Vorname,setVorname]=useState("")
+ const [Nachname,setNachname]=useState("")
+ const [Mailadress,setMailadress] =useState("")
+ const [Telefon,setTelefon]=useState("")
+ const [Strasse,setStrasse]=useState("")
+ const [Haousenumber,setHaousenumber]=useState("")
+ const [Stadt,setStadt]=useState("")
+ const [PostCode,setPostCode]=useState("")
+ const [Payment,setPayment]=useState("")
+ const [Info,setInfo]=useState("")
 
 
 
-const db = mysql.createPool({
-    host:"localhost",
-    user: "root",
-    password:"password",
-    database:"VERKAUFDataBase", 
-});
-const fileUpload = require('express-fileupload');
- 
 
-app.use(express())
-app.use(fileUpload());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+ const submitReview=  (e)=>{
+  
+
+  e.preventDefault();
+  try{
+     Axios.post("https://api.verkauf.tiptopmobile.de/api/get/insert",{
+      vorname : Vorname,
+      nachname : Nachname,
+      mailadress : Mailadress,
+      telefon : Telefon,
+      strasse: Strasse,
+      housenumber : Haousenumber,
+      stadt : Stadt,
+      postCode : PostCode,
+      payment : Payment,
+      info:Info,
+      phoneName: SelectedPhoneName,
+      PhonePrice: priceText
+    })
+    navigate("/success");
+     
+   
+  }catch(err){
+
+    console.log(err);
+  }
+  
+
+ };
 
 
   
-app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://verkaufen.tiptopmobile.de"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-
-
-
-app.post('/upload/:id', (req, res) => {
-  const pid = req.params.id;
-  console.log(pid)
-  if (req.files === null) {
-    return res.status(400).json({ msg: 'No file uploaded' });
+function CheckerPayment(e){
+  if (e==="PayPal Account *")
+  {setimgShow(paypal) }
+  else{
+    setimgShow(banktrans)
   }
+}
+  
+  const [selected, setSelected] = useState("Paypal Account *");
+  
+const[priceText, setPriceText]=useState(' ')
+const [SelectedPhoneName,setSelectedPhoneName]=useState("")
+  const[phoneModelList, setPhoneBrandList] = useState([])
+  useEffect(()=>{
 
-  const file = req.files.file;
+    async function get (){
+      await  Axios.get("https://api.verkauf.tiptopmobile.de/api/detail/"+phoneId).then((response)=>{
+  
+        setPhoneBrandList(response.data)
+        
+        setSelectedPhoneName(response.data[0]?.PhonesName)
+        setPriceText(response.data[0]?.PhonePrice_1 + "€ Hervorragendn")
 
-  file.mv(`${__dirname}/public/phoneImg/${pid+".png"}`, err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+   console.log(response.data[0]?.PhonePrice_1)
+     })
+  
     }
 
-    res.json({ fileName: file.name, filePath: `/phoneImg/${file.name}` });
-  });
-});
+   get()
 
-app.post("/api/phone/insert", (req,res)=>{
-
-  const PhoneName =req.body.phoneName
-  const PhoneBrand = req.body.phoneBrand
-  const PhonePrice_1 = req.body.phonePrice_1
-  const PhonePrice_2 = req.body.phonePrice_2
-  const PhonePrice_3 = req.body.phonePrice_3
-  const PhonePrice_4 = req.body.phonePrice_4
-
-try{
-
-  const sqlInsert=
-  "INSERT INTO Phones (PhonesBrand,PhonesName,PhonePrice_1,PhonePrice_2,PhonePrice_3,PhonePrice_4) Values (?,?,?,?,?,?);"
-   db.query(sqlInsert,[PhoneBrand,PhoneName,PhonePrice_1,PhonePrice_2,PhonePrice_3,PhonePrice_4])
+  },[1])
 
 
-  alert("successful insert")
-
-}catch(error){
-console.log(error)
-
-}
- 
+  return(
   
- 
-})
-
-app.delete("/api/delete/:id", (req,res)=>{
-  const PhoneId = req.params.id;
-
-
-try{
-
-  const sqlDelete=
-  "DELETE FROM phones WHERE id = ?;"
-   db.query(sqlDelete,[PhoneId])
-
-
-  alert("successful update")
-
-}catch(error){
-console.log(error)
-
-}
- 
-  
- 
-})
-
-app.post("/api/update/:id", (req,res)=>{
-  const PhoneId = req.params.id;
-  const PhoneName =req.body.phoneName
-  const PhoneBrand = req.body.phoneBrand
-  const PhonePrice_1 = req.body.phonePrice_1
-  const PhonePrice_2 = req.body.phonePrice_2
-  const PhonePrice_3 = req.body.phonePrice_3
-  const PhonePrice_4 = req.body.phonePrice_4
-
-try{
-
-  const sqlUpdate=
-  "UPDATE Phones SET PhonesBrand = ?,PhonesName = ?,PhonePrice_1 = ?,PhonePrice_2 = ?,PhonePrice_3 = ?,PhonePrice_4 = ?  WHERE id = ?;"
-   db.query(sqlUpdate,[PhoneBrand,PhoneName,PhonePrice_1,PhonePrice_2,PhonePrice_3,PhonePrice_4,PhoneId])
-
-
-  alert("successful update")
-
-}catch(error){
-console.log(error)
-
-}
- 
-  
- 
-})
-
-app.post("/api/get/insert", (req,res)=>{
-
-    const Vorname=req.body.vorname
-    const Nachname=req.body.nachname
-    const Mailadress=req.body.mailadress
-    const Telefon=req.body.telefon
-    const Strasse=req.body.strasse
-    const Haousenumber=req.body.housenumber
-    const Stadt=req.body.stadt
-    const PostCode=req.body.postCode
-    const Payment=req.body.payment
-    const Info=req.body.info
-    const SelectedPhoneName = req.body.phoneName
-    const PhonePrice = req.body.PhonePrice
-
-
-try{
-
-    const sqlInsert=
-    "INSERT INTO phone_form (vorName,nachName,mailAdress,telefon,strasse,houseNumber,stadt,postCode,payment,info,phoneName,phonePrice) Values (?,?,?,?,?,?,?,?,?,?,?,?);"
-     db.query(sqlInsert,[Vorname,Nachname,Mailadress,Telefon,Strasse,Haousenumber,Stadt,PostCode,Payment,Info,SelectedPhoneName,PhonePrice])
-  
-
-    alert("successful insert")
-
-}catch(error){
-
-}
-   
+    <div>  { show ? 
+      <div>
+      <h1> </h1>
+      
+    <main className="container">
     
-   
-})
+    <div className="left-column">
+    
+    <img src={`https://api.verkauf.tiptopmobile.de/phoneImg/${phoneId}.png`} alt='phonepic'/>
+    
+    </div>
+    
+    <div className="right-column">
+    
+    <div className="product-description">
+    <span>{phoneModelList[0]?.PhonesBrand}</span>
+    <h1>{phoneModelList[0]?.PhonesName}</h1>
+    <p>The preferred choice of a vast range of acclaimed DJs. Punchy, bass-focused sound and high isolation. Sturdy headband and on-ear cushions suitable for live performance
+      
+    </p>
+    </div>
+    
+    <div className="product-configuration">
+    
+    
+    
+    <div className="cable-config">
+     <span>Zustandsbeschreibung</span>
+    <br></br>
+    <br></br>
+     <div className="cable-choose">
+       <button id="1" value="1"  onClick={()=>setPriceText(phoneModelList[0]?.PhonePrice_1 +"€ Hervorragend" )}  required>Hervorragend</button>
+       <button id="2" value="2" onClick={()=>setPriceText(phoneModelList[0]?.PhonePrice_2 +"€ Gut")}  >Gut</button>
+       <button id="3" value="3"  onClick={()=>setPriceText(phoneModelList[0]?.PhonePrice_3 +"€ Genutzt")}  >Genutzt</button>
+       <button id="4" value="4"  onClick={()=>setPriceText(phoneModelList[0]?.PhonePrice_4 +"€ in Ordnung")} >in Ordnung</button>
+     </div>
+    
+     <a href="#">Den Status meines Telefons kategorisieren</a>
+    </div>
+    </div>
+    
+    <div className="product-price">
+    <span>{priceText}</span>
+    <a onClick={()=>setShow(!show)} className="cart-btn">Bestätigen</a>
 
-
-app.get("/api/get/:brand",(req,res)=>{
-    const brand = req.params.brand;
- 
   
-
-    const sqlSelect= "SELECT * FROM Phones WHERE PhonesBrand LIKE ?;"
-
-    db.query(sqlSelect,[brand +'%'],(err,result)=>{
-
-        res.send(result);  
-   
-    });
-});
-app.get("/api/get",(req,res)=>{
-
-  const sqlSelect= "SELECT * FROM Phones"
-
-  db.query(sqlSelect,(err,result)=>{
-
-      res.send(result);  
- 
-  });
-});
-
-app.get("/api/form",(req,res)=>{
-
-  const sqlSelect= "SELECT * FROM phone_form"
-
-  db.query(sqlSelect,(err,result)=>{
-
-      res.send(result);  
- 
- 
-  });
-});
-
-app.get("/api/detail/:phoneId",(req,res)=>{
-    const phoneId = req.params.phoneId;
- 
+    </div>
+    </div>
+    </main>
+    
   
-
-    const sqlSelect= "SELECT * FROM Phones WHERE id = ?;"
-
-    db.query(sqlSelect,[phoneId],(err,result)=>{
-
-        res.send(result);  
-   
-    });
-});
-
-
-
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    key: "userId",
-    secret: "subscribe",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      expires: 60 * 60 * 24,
-    },
-  })
-);
-
-
-
-app.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) {
-      console.log(err);
-    }
-
-    db.query(
-      "INSERT INTO users (username, password) VALUES (?,?)",
-      [username, hash],
-      (err, result) => {
-        console.log(err);
-      }
-    );
-  });
-});
-
-app.get("/logincheck", (req, res) => {
-  if (req.session.user) {
-    res.send({ loggedIn: true, user: req.session.user });
-  } else {
-    res.send({ loggedIn: false });
-  }
-});
-
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  db.query(
-    "SELECT * FROM users WHERE username = ?;",
-    username,
-    (err, result) => {
-      if (err) {
-      return  res.send({ err: err });
-      }
-
-      if (result.length > 0) {
-        bcrypt.compare(password, result[0].password, (error, response) => {
-          if (response) {
-            req.session.user = result;
+      <div className="row">
+          <div className="col">
+            <h2>Häufig gestellte Fragen <b>Fragen </b></h2>
+            <div className="tabs">
+              <div className="tab">
+                <input type="checkbox" id="chck1" className="inputTab" />
+                <label className="tab-label" htmlFor="chck1">Kann das vorläufige Preisangebot vom endgültigen Preisangebot abweichen? Wieso den?</label>
+                <div className="tab-content">
+                Ja vielleicht. Wir testen Ihr Gerät ausführlich vom Lautsprecher bis zum Bildschirm, von der Speicherkarte bis zum Ladeanschluss. 
+                Während dieses Tests können wir einige Fehlfunktionen feststellen, die Ihnen nicht bekannt sind. In diesem Fall kann das endgültige Preisangebot abweichen.
+                </div>
+              </div>
+              <div className="tab">
+                <input type="checkbox" id="chck2" className="inputTab"/>
+                <label className="tab-label" htmlFor="chck2">Wann erhalte ich die Zahlung? </label>
+                <div className="tab-content">
+                Nachdem wir Ihr Gerät überprüft haben, senden wir Ihnen eine SMS mit unserem endgültigen Angebot. 
+                Wenn Sie dies genehmigen, überweisen wir Ihr Geld innerhalb von 1 Tag auf Ihr Konto.
+                </div>
+              </div>
+              <div className="tab">
+                <input type="checkbox" id="chck3" className="inputTab"/>
+                <label className="tab-label" htmlFor="chck3">Was passiert, wenn ich den Endpreis nicht bestätige? </label>
+                <div className="tab-content">
+                Wir senden Ihr Gerät per Kurier an Sie zurück.
+                </div>
+              </div>
+              <div className="tab">
+                <input type="checkbox" id="chck4" className="inputTab"/>
+                <label className="tab-label" htmlFor="chck4">Wie schicke ich mein Handy zu dir? </label>
+                <div className="tab-content">
+                Sie können Ihr Etikett ausdrucken und über die DHL-Seite zur Etikettenerstellung an uns senden. oder Sie überlassen es unseren Filialen.
+                </div>
+              </div>
+            </div>
+          </div>
          
-            res.send(result);
-          } else {
-            res.send({ message: "Wrong username/password combination!" });
-          }
-        });
-      } else {
-        res.send({ message: "User doesn't exist" });
-      }
-    }
-  );
-});
+        </div>
 
-app.use(express.static('public')); 
-app.use('/phoneImg', express.static('images'));
+      </div>:
+    <div>
+    <div className="form-style-5">
 
-app.listen(3001);
+      <form action="" method="post" onSubmit={submitReview} >
+        <fieldset>
+          <legend><span className="number">1</span> PERSÖNLICHE DATEN
+          </legend>
+          <input type="text" name='Vorname'  placeholder="Nachname *" onChange={(e) => {setNachname(e.target.value)} } />
+          <input type="text" name='Nachname' placeholder="Vorname *"  onChange={(e) => setVorname(e.target.value) } />
+          <input type="email" name='Mailadress'  placeholder="Your Email *" onChange={(e) => setMailadress(e.target.value) } />
+          <input type="text"  name='Telefon' placeholder="Telefon *" onChange={(e) => setTelefon(e.target.value) } />
+          <legend><span className="number">2</span> ADRESSE
+        </legend>
+          <input type="text"  name='Strasse' placeholder="Strasse *" onChange={(e) => setStrasse(e.target.value) } />
+          <input type="text"  name='Haousenumber' placeholder="Hausnummer *" onChange={(e) => setHaousenumber(e.target.value) } />
+          <input type="text"  name='Stadt' placeholder="Stadt *" onChange={(e) => setStadt(e.target.value) }/>
+          <input type="text"  name='PostCode' placeholder="Postleitzahl *" onChange={(e) => setPostCode(e.target.value) }  />
+
+          <legend><span className="number">3</span> Additional Info</legend>
+          <br />
+          <img src={dhllogo} width="200"  alt='dhllogo'/>
+          <label htmlFor="job">Im Anschluss Kannst Du Deınen DHL Versandscheın Erstellen. - 0,00€</label>
+          <br />
+          <label htmlFor="job">WIe Möchtest Du Ausbezahlt Werden?:</label>
+          
+
+
+          <select id="Payment" onChange={ (e) => {setSelected(e.target.value || null);
+          CheckerPayment(e.target.value );}}>
+            <optgroup label="Payment" >
+              <option label="Paypal" value="PayPal Account *"  selected> </option>
+              <option label="Auszahlung per Überweisung" value="IBAN Number *"></option>
+            </optgroup>
+          </select>     
+
+         
+          <label htmlFor="job">{selected}:</label>
+       <img src = {imgshow} width="200" alt='dhl logo' ></img>
+       <input type="text" name='Payment' placeholder={selected}  onChange={(e) => setPayment(e.target.value) } />
+
+
+        </fieldset>
+        <fieldset>
+          <legend><span className="number">4</span> Zusätzliche Information</legend>
+          <textarea name='Info' placeholder="Wenn Sie uns etwas mitteilen möchten, schreiben Sie es hier. (Optional) " onChange={(e) => setInfo(e.target.value) }  />
+        </fieldset>
+        <ReCAPTCHA
+    sitekey="6LfmZaAhAAAAAOB6N7JvQTJg6zilAcgtdHnpazNb"
+ 
+  />
+ <br></br>
+
+ <button type = "submit" onSubmit={submitReview} > Bestätigen</button>
+    
+        
+ 
+      </form>
+      
+    </div>
+</div>}</div>
+    
+  )
+
+
+
+
+
+}
+
+export default PDetailpage
